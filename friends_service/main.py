b2021b11fdd8ad3f6e3203645f36ea2a6d121c1e
printeromg <PrinterOMG@ya.ru>
2024-03-03 14:39:17 +0300

    Update docs urls

--- friends_service/main.py
+++ friends_service/main.py
@@ -2,6 +2,6 @@ from fastapi import FastAPI
 
 from api.routers import router as main_router
 
-app = FastAPI()
+app = FastAPI(docs_url='/api/docs', redoc_url='/api/redoc', openapi_url='/api/openapi.json')
 
 app.include_router(main_router, prefix='/api/v1')
